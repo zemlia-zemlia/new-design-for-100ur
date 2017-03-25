4b8f239e9222696cb6060ae0d5c2b1193308e658
root <root@100yuristov.com>
2017-03-25 15:27:15 +0300

    страница вопроса

--- protected/views/question/view.php
+++ protected/views/question/view.php
@@ -92,41 +92,36 @@ if($model->description) {
 <?php if(Yii::app()->user->role == User::ROLE_ROOT || ($model->authorId == Yii::app()->user->id && ($model->price==0 || $model->payed == 0))):?>
 
 
-		<h3 class="vert-margin20 header-block-light-grey"><strong> 100% гарантия получения ответа </strong></h3>
+		<h3 class="header-block-light-grey"><strong> 100% гарантия получения ответа </strong></h3>
 
 		<table class="table center-align small table-bordered alert alert-info">
                 <tr>
-                    <th class="center-align">Тариф:</th>
-                    <th class="center-align">Бронзовый</th>
-                    <th class="center-align">Серебрянный</th>
-                    <th class="center-align">Золотой</th>
+                    <th class="center-align" style="width: 33%">Бронза</th>
+                    <th class="center-align" style="width: 33%">Серебро</th>
+                    <th class="center-align" style="width: 33%">Золото</th>
                 </tr>
                 <tr>
 
                 </tr>
                 <tr  class="warning">
-                    <td>Гарантировано ответов</td>
-                    <td>1</td>
-                    <td>3</td>
-                    <td>5</td>
+                    <td>1<br/><span class="mutted">гарантированный ответ</span></td>
+                    <td>3<br/><span class="mutted">гарантированных ответа</span></td>
+                    <td>5<br/><span class="mutted">гарантированных ответов</span></td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td>Один гарантированный ответ одного из юристов портала</td>
+                    <td>Один гарантированный ответ одного из проверенных юристов портала</td>
                     <td>Три гарантированных ответа юристов, позволят Вам понять, как решить проблему</td>
                     <td>Минимум пять гарантированных ответов юристов. Мнения нескольких юристов. Гарантия полного и подробного разбора ситуации.</td>
                 </tr>
                 <tr class="success">
-                    <td>Цена</td>
                     <td>125 руб.</td>
                     <td>295 руб.</td>
                     <td>455 руб.</td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td><?php echo CHtml::link('Выбрать', Yii::app()->createUrl('question/upgrade', array('id'=>$model->id, 'level'=>Question::LEVEL_1)), array('class'=>'btn btn-success btn-block'));?></td>
-                    <td><?php echo CHtml::link('Выбрать', Yii::app()->createUrl('question/upgrade', array('id'=>$model->id, 'level'=>Question::LEVEL_2)), array('class'=>'btn btn-success btn-block'));?></td>
-                    <td><?php echo CHtml::link('Выбрать', Yii::app()->createUrl('question/upgrade', array('id'=>$model->id, 'level'=>Question::LEVEL_3)), array('class'=>'btn btn-success btn-block'));?></td>
+                    <td><?php echo CHtml::link('Выбрать', Yii::app()->createUrl('question/upgrade', array('id'=>$model->id, 'level'=>Question::LEVEL_1)), array('class'=>'btn btn-warning btn-block'));?></td>
+                    <td><?php echo CHtml::link('Выбрать', Yii::app()->createUrl('question/upgrade', array('id'=>$model->id, 'level'=>Question::LEVEL_2)), array('class'=>'btn btn-warning btn-block'));?></td>
+                    <td><?php echo CHtml::link('Выбрать', Yii::app()->createUrl('question/upgrade', array('id'=>$model->id, 'level'=>Question::LEVEL_3)), array('class'=>'btn btn-warning btn-block'));?></td>
                 </tr>
             </table>
 
