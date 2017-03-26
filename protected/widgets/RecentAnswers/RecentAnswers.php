f127256d46648add0e10de93c325510defc6a442
Michael <Michael@192.168.1.43>
2017-03-26 19:45:40 +0300

    в блоке ответов теперь отображаются ответы от уникальных юристов

--- protected/widgets/RecentAnswers/RecentAnswers.php
+++ protected/widgets/RecentAnswers/RecentAnswers.php
@@ -1,22 +1,44 @@
 <?php
 
-// виджет для вывода последних ответов
+/**
+ *  виджет для вывода последних ответов, юристы должны быть уникальными
+ */
 
 class RecentAnswers extends CWidget
 {
     public $template = 'default'; // представление виджета по умолчанию
-    public $cacheTime = 600; // время кеширования
+    public $cacheTime = 0; // время кеширования
     public $limit = 5;
     
     public function run()
     {
-        $criteria = new CDbCriteria();
-        $criteria->limit = $this->limit;
-        $criteria->order = 'datetime DESC';
-        $criteria->with = array('question', 'author');
         
-        $answers = Answer::model()->findAll($criteria);
-                
+        // выберем ответы за последний месяц, с вопросами и авторами
+        $answersRows = Yii::app()->db->cache($this->cacheTime)->createCommand()
+                ->select('q.title questionTitle, q.id questionId, q.price questionPrice, q.payed questionPayed, a.answerText, a.datetime answerTime, a.authorId, u.name authorName, u.name2 authorName2, u.lastName authorLastName')
+                ->from('{{answer}} a')
+                ->leftJoin('{{user}} u', 'a.authorId = u.id')
+                ->leftJoin('{{question}} q', 'q.id = a.questionId')
+                ->where('a.datetime>NOW()-INTERVAL 30 DAY AND a.status!=2')
+                ->order('answerTime DESC')
+                ->queryAll();
+        
+       
+        $answers = array();
+        $authorId = 0;
+        
+        // нам нужны ответы от уникальных авторов, выберем их в массив $answers
+        foreach($answersRows as $row) {
+            if($row['authorId']!=$authorId && !$answers[$row['authorId']]) {
+                $answers[$row['authorId']] = $row;
+                $authorId = $row['authorId'];
+            }
+            // в выходном массиве нужно не более $limit элементов
+            if(sizeof($answers) == $this->limit) {
+                break;
+            }
+        }
+                            
         $this->render($this->template, array(
             'answers'  =>  $answers,
         ));
--- protected/widgets/RecentAnswers/views/default.php
+++ protected/widgets/RecentAnswers/views/default.php
@@ -13,38 +13,34 @@ if(empty($answers) || sizeof($answers)==0) {
 <?php foreach($answers as $answer): ?>
     <div class="answer-item-panel">
 <p>
-    <?php echo CHtml::link(CHtml::encode($answer->question->title), Yii::app()->createUrl('question/view',array('id'=>$answer->question->id)));?>
-        <?php if($answer->question->price!=0 && $answer->question->payed == 1):?>
+    <?php echo CHtml::link(CHtml::encode($answer['questionTitle']), Yii::app()->createUrl('question/view',array('id'=>$answer['questionId'])));?>
+        <?php if($answer['questionPrice']!=0 && $answer['questionPayed'] == 1):?>
             <span class="label label-primary">VIP</span>
             <?php endif;?>
 </p>
 
 <div class="row">
     <div class="col-md-4">
-		<?php if($answer->author):?>
-        <img src="<?php echo $answer->author->getAvatarUrl();?>" class="img-responsive img-bordered" />
-		<?php endif;?>
+        <?php if($answer['authorId'] && ($author=User::model()->cache(600)->findByPk($answer['authorId'])) instanceof User):?>
+            <img src="<?php echo $author->getAvatarUrl();?>" class="img-responsive img-bordered" />
+        <?php endif;?>
     </div>
     <div class="col-md-8">
         <div class="answer-item-info">
-            <?php if($answer->datetime):?>
-            <img src="/pics/2017/icon_calendar_green.png" alt="" /> <?php echo CustomFuncs::niceDate($answer->datetime, false);?>
+            <?php if($answer['answerTime']):?>
+            <img src="/pics/2017/icon_calendar_green.png" alt="" /> <?php echo CustomFuncs::niceDate($answer['answerTime'], false);?>
             <br />
             <?php endif;?>
-            <?php if($answer->authorId):?>
+            <?php if($answer['authorId']):?>
                 <img src="/pics/2017/icon_yurist_green.png" alt="" />
-                    <?php if($answer->author->settings && $answer->author->settings->alias):?>
-                        <?php echo CHtml::encode($answer->author->settings->alias);?>
-                    <?php else:?>
-                        <?php echo $answer->author->getShortName();?>
-                    <?php endif;?>
+                <?php echo $answer['authorLastName'] . ' ' . mb_substr($answer['authorName'], 0, 1, 'utf-8') . '.' . mb_substr($answer['authorName2'], 0, 1, 'utf-8') . '.';?> 
             <?php endif;?>
         </div>
     </div>
 </div>
 
 <img src="/pics/2017/arrow_list_blue.png" alt="" /> 
-<?php echo nl2br(mb_substr(CHtml::encode($answer->answerText),0,100,'utf-8'));?>...
+<?php echo nl2br(mb_substr(CHtml::encode($answer['answerText']),0,100,'utf-8'));?>...
 <br />
         
 </div>
