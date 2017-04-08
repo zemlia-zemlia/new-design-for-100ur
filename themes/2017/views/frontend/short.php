ce057497fa40c6d5b5b8f6cff3f1c562a1de3be4
Michael <Michael@192.168.1.43>
2017-04-08 14:36:07 +0300

    открыли блог в robots.txt

--- themes/2017/views/frontend/short.php
+++ themes/2017/views/frontend/short.php
@@ -1,11 +1,10 @@
 <!doctype html>
 <html lang="ru">
 <head>
+<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php echo CHtml::encode($this->pageTitle); ?></title>
-
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
 
 <?php 
