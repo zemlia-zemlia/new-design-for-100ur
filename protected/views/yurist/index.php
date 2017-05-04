91a530d4115a81e44d74354aedb1f5bc5d4d66c0
Michael Krutikov <mukrytikov@atomstandard.ru>
2017-05-04 12:10:24 +0300

    мелкие правки профиля пользователя

--- protected/views/yurist/index.php
+++ protected/views/yurist/index.php
@@ -8,6 +8,15 @@ $this->setPageTitle($pageTitle . Yii::app()->name);
 
 <h1 class="vert-margin30">Юристы и Адвокаты</h1>
 
+<div class="alert alert-success">
+    <h4>Вы юрист?</h4>
+    <p>
+        Хотите получать клиентов, отвечая на вопросы на нашем сайте?
+        <br />
+        Напишите нам: admin@100yuristov.com
+    </p>
+</div>
+
 <?php $this->widget('zii.widgets.CListView', array(
 	'dataProvider'  =>  $yuristsDataProvider,
 	'itemView'      =>  '_view',
