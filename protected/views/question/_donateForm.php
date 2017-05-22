9e20b09a161f6b638f7d6ce088e91d96e852fcef
Michael Krutikov <mukrytikov@atomstandard.ru>
2017-05-22 10:11:17 +0300

    default donate 195

--- protected/views/question/_donateForm.php
+++ protected/views/question/_donateForm.php
@@ -3,4 +3,4 @@
     $successUrl = (isset($successUrl))?urlencode($successUrl):urlencode(Yii::app()->urlManager->baseUrl);
 ?>
 
-<iframe frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/quickpay/shop-widget?account=410012948838662&quickpay=shop&payment-type-choice=on&mobile-payment-type-choice=on&writer=seller&targets=<?php echo $target;?>&targets-hint=&default-sum=50&button-text=01&successURL=<?php echo $successUrl;?>" width="450" height="198"></iframe>
+<iframe frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/quickpay/shop-widget?account=410012948838662&quickpay=shop&payment-type-choice=on&mobile-payment-type-choice=on&writer=seller&targets=<?php echo $target;?>&targets-hint=&default-sum=195&button-text=01&successURL=<?php echo $successUrl;?>" width="450" height="198"></iframe>
