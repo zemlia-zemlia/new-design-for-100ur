6697a1cc7c3e2038f8cd138017ac550379dd3c72
Michael <Michael@192.168.1.43>
2017-07-04 07:09:45 +0300

    начало работы с апи

--- protected/models/Leadsource100.php
+++ protected/models/Leadsource100.php
@@ -13,133 +13,124 @@
  * @property string $appId
  * @property string $secretKey
  */
-class Leadsource100 extends CActiveRecord
-{
-	/**
-	 * Returns the static model of the specified AR class.
-	 * @param string $className active record class name.
-	 * @return Leadsource the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+class Leadsource100 extends CActiveRecord {
 
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return '{{leadsource100}}';
-	}
+    /**
+     * Returns the static model of the specified AR class.
+     * @param string $className active record class name.
+     * @return Leadsource the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('name', 'required'),
-			array('name, description', 'length', 'max'=>255),
-                    	array('officeId, noLead, active', 'numerical', 'integerOnly'=>true),
-			// The following rule is used by search().
-			// Please remove those attributes that should not be searched.
-			array('id, name, description', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return '{{leadsource100}}';
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-                    'office'    =>  array(self::BELONGS_TO, 'Office', 'officeId'),
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('name', 'required'),
+            array('name, description', 'length', 'max' => 255),
+            array('officeId, noLead, active', 'numerical', 'integerOnly' => true),
+            // The following rule is used by search().
+            // Please remove those attributes that should not be searched.
+            array('id, name, description', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id'            =>  'ID',
-			'name'          =>  'Название',
-			'description'   =>  'Описание',
-                        'officeId'      =>  'Офис',
-                        'noLead'        =>  'Клиенты сразу приходят на консультацию',
-                        'active'        =>  'Активность',
-                        'appId'         =>  'ID источника для API',
-                        'secretKey'     =>  'Секретный ключ для API',
-		);
-	}
-        
-        /**
-         *  возвращает массив источников лидов, ключами которого являются ID, а значениями - названия
-         * 
-         * @param boolean $showInactive показывать неактивные источники
-         * @return Array массив источников лидов (id => name)
-         */
-        
-        static public function getSourcesArray($showInactive = true)
-        {
-            $attributes = array();
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'office' => array(self::BELONGS_TO, 'Office', 'officeId'),
+        );
+    }
 
-            if($showInactive == false) {
-                $attributes['active'] = 1;
-            } 
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'id' => 'ID',
+            'name' => 'Название',
+            'description' => 'Описание',
+            'officeId' => 'Офис',
+            'noLead' => 'Клиенты сразу приходят на консультацию',
+            'active' => 'Активность',
+            'appId' => 'ID источника для API',
+            'secretKey' => 'Секретный ключ для API',
+        );
+    }
 
-            //CustomFuncs::printr($attributes);
-            $sources = self::model()->cache(3600)->findAllByAttributes($attributes);
+    /**
+     *  возвращает массив источников лидов, ключами которого являются ID, а значениями - названия
+     * 
+     * @param boolean $showInactive показывать неактивные источники
+     * @return Array массив источников лидов (id => name)
+     */
+    static public function getSourcesArray($showInactive = true) {
+        $attributes = array();
 
-            $sourcesArray = array(null=>'Нет');
-            foreach($sources as $source) {
-                $sourcesArray[$source->id] = $source->name;
-            }
-            return $sourcesArray;
+        if ($showInactive == false) {
+            $attributes['active'] = 1;
         }
 
-        /**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// Warning: Please modify the following code to remove attributes that
-		// should not be searched.
+        //CustomFuncs::printr($attributes);
+        $sources = self::model()->cache(3600)->findAllByAttributes($attributes);
 
-		$criteria=new CDbCriteria;
-
-		$criteria->compare('id',$this->id);
-		$criteria->compare('name',$this->name,true);
-                $criteria->compare('office',$this->office,true);
-		$criteria->compare('description',$this->description,true);
-
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
-        
-        /**
-         * Генерирует appId
-         * @return integer appId
-         */
-        public function generateAppId()
-        {
-            $this->appId = mt_rand(1000000, 9999999);
+        $sourcesArray = array(null => 'Нет');
+        foreach ($sources as $source) {
+            $sourcesArray[$source->id] = $source->name;
         }
-        
-        /**
-         * Генерирует secretKey
-         * @return string secretKey
-         */
-        public function generateSecretKey()
-        {
-            $this->secretKey = md5($this->id . mt_rand(1000000, 9999999) . time());
-        }
+        return $sourcesArray;
+    }
+
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+     */
+    public function search() {
+        // Warning: Please modify the following code to remove attributes that
+        // should not be searched.
+
+        $criteria = new CDbCriteria;
+
+        $criteria->compare('id', $this->id);
+        $criteria->compare('name', $this->name, true);
+        $criteria->compare('office', $this->office, true);
+        $criteria->compare('description', $this->description, true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
+    /**
+     * Генерирует appId
+     * @return integer appId
+     */
+    public function generateAppId() {
+        $this->appId = mt_rand(1000000, 9999999);
+    }
+
+    /**
+     * Генерирует secretKey
+     * @return string secretKey
+     */
+    public function generateSecretKey() {
+        $this->secretKey = md5($this->id . mt_rand(1000000, 9999999) . time());
+    }
+
 }
