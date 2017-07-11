e0c6a8fab3f48b4434c26a5704ea192362b4607c
Michael Krutikov <mukrytikov@atomstandard.ru>
2017-07-11 17:51:29 +0300

    библиотека клиент апи

--- protected/extensions/StoYuristovClient/StoYuristovClient.php
+++ protected/extensions/StoYuristovClient/StoYuristovClient.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace stoyuristov;
+//namespace stoyuristov;
 
 /**
  * Клиент для работы с API сервиса 100 Юристов
@@ -12,16 +12,16 @@ class StoYuristovClient {
     protected $_secretKey; // секретный ключ кампании
     protected $_curlLink; // линк Curl
     protected $_signature; // подпись запроса
-    protected $_apiUrlTest = 'http://100juristov/api/sendLead';
-    protected $_apiUrl = 'https://100yuristov.com/api/sendLead/';
-    
+    protected $_apiUrlTest = 'http://100yuristov/api/sendLead/';
+//    protected $_apiUrl = 'https://100yuristov.com/api/sendLead/';
+    protected $_apiUrl = '';
+    protected $_testMode; // 0|1 Включение / выключение тестового режима
     // параметры лида
     public $name;
     public $phone;
     public $question;
     public $town;
     public $email;
-    public $testMode = 0; // 0|1 Включение / выключение тестового режима
 
     /**
      * Конструктор
@@ -29,12 +29,14 @@ class StoYuristovClient {
      * @param integer $appId
      * @param string $secretKey
      */
-    public function __construct($appId, $secretKey) {
+    public function __construct($appId, $secretKey, $testMode = 0) 
+    {
         $this->_appId = $appId;
         $this->_secretKey = $secretKey;
+        $this->_testMode = $testMode;
 
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->_apiUrlTest);
+        curl_setopt($ch, CURLOPT_URL, ($this->_testMode === 1) ? $this->_apiUrlTest : $this->_apiUrl);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HEADER, false);
         curl_setopt($ch, CURLOPT_POST, 1);
@@ -46,7 +48,8 @@ class StoYuristovClient {
     /**
      * Вычисление сигнатуры
      */
-    protected function _calculateSignature() {
+    protected function _calculateSignature() 
+    {
         $this->_signature = md5($this->name . $this->phone . $this->town . $this->question . $this->_appId . $this->_secretKey);
     }
 
@@ -54,48 +57,48 @@ class StoYuristovClient {
      * Проверка данных перед отправкой в API на стороне клиента
      * @return mixed Результат проверки. true если все в 
      */
-    protected function _validate() {
+    protected function _validate() 
+    {
         $errors = array();
-        
-        if(!$this->name) {
+
+        if (!$this->name) {
             $errors[] = "Не указано имя";
         }
-        if(!$this->phone) {
+        if (!$this->phone) {
             $errors[] = "Не указан номер телефона";
         }
-        if(!$this->question) {
+        if (!$this->question) {
             $errors[] = "Не указан текст вопроса";
         }
-        if(!$this->town) {
+        if (!$this->town) {
             $errors[] = "Не указан город";
         }
-        
-        if(empty($errors)) {
+
+        if (empty($errors)) {
             return true;
         } else {
             return $errors;
         }
     }
-    
+
     /**
      * Возвращает массив параметров для POST запроса
      * 
      * @return array Массив параметров
      */
-    protected function _getParams()
+    protected function _getParams() 
     {
         return array(
-            'name'          => $this->name,
-            'phone'         => $this->phone,
-            'email'         => $this->email,
-            'town'          => $this->town,
-            'question'      => $this->question,
-            'appId'         => $this->_appId,
-            'signature'     => $this->_signature,
+            'name' => $this->name,
+            'phone' => $this->phone,
+            'email' => $this->email,
+            'town' => $this->town,
+            'question' => $this->question,
+            'appId' => $this->_appId,
+            'signature' => $this->_signature,
         );
     }
 
-    
     /**
      * Отправляет лид в api
      * 
@@ -103,34 +106,29 @@ class StoYuristovClient {
      * * code - код ответа. 200 - ОК, остальные коды - ошибки
      * * message - сообщение от api
      */
-    public function send() {
+    public function send() 
+    {
         // проверяем данные
         if (($errors = $this->_validate()) !== true) {
             return array("message" => "Некорректные данные", 'errors' => $errors);
         }
-        
+
+        // вычисляем сигнатуру
+        $this->_calculateSignature();
+
         // Создаем запрос с POST параметрами
         curl_setopt($this->_curlLink, CURLOPT_POSTFIELDS, $this->_getParams());
         $jsonResponse = curl_exec($this->_curlLink);
         $curlInfo = curl_getinfo($this->_curlLink);
         curl_close($this->_curlLink);
 
-        if ($jsonResponse !== false && $curlInfo['http_code'] == 200) {
-//          CustomFuncs::printr($curlInfo);
-//          echo 'Ответ сервера';
-
+        if ($jsonResponse !== false) {
+            // Возвращаем ответ от API в виде ассоциативного массива (code => код_ответа, message => текст ответа)
             $response = json_decode($jsonResponse, true);
-            //echo '<textarea style="width:500px;" rows="10">' . $jsonResponse . '</textarea>';
-            //CustomFuncs::printr($jsonResponse);
-//          CustomFuncs::printr($response);
-//          exit;
-
             return $response;
-            
         } else {
             return array("message" => "Ошибка при отправке лида на сервер");
         }
-        
     }
 
 }
