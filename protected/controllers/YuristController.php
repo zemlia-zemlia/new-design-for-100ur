1c0199b476d6c6fb8868427cae30cf80772e11ad
Michael <Michael@192.168.1.43>
2017-08-19 17:03:49 +0300

    новые шаблоны

--- protected/controllers/YuristController.php
+++ protected/controllers/YuristController.php
@@ -1,62 +1,55 @@
 <?php
 
-class YuristController extends Controller
-{
+class YuristController extends Controller {
 
-	public $layout='//frontend/main';
+    public $layout = '//frontend/question';
 
-        
-        
-	/**
-	 * @return array action filters
-	 */
-	public function filters()
-	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-			//'postOnly + delete', // we only allow deletion via POST request
-		);
-	}
+    /**
+     * @return array action filters
+     */
+    public function filters() {
+        return array(
+            'accessControl', // perform access control for CRUD operations
+                //'postOnly + delete', // we only allow deletion via POST request
+        );
+    }
 
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-            return array(
-                array('allow', // allow all users 
-                        'actions'=>array('index'),
-                        'users'=>array('*'),
-                ),
-                array('deny',  // deny all users
-                        'users'=>array('*'),
-                ),
-            );
-	}
-        
-        
-	public function actionIndex()
-	{
-            $criteria = new CDbCriteria;
-            
-            $criteria->order = "karma DESC";
-            $criteria->with = array("settings", "town", "town.region", "categories", "answersCount");
-            $criteria->addColumnCondition(array('active100'=>1));
-            $criteria->addColumnCondition(array('avatar!'=>''));
-            $criteria->addCondition("role = " . User::ROLE_JURIST);
-        
-            $yuristsDataProvider = new CActiveDataProvider('User', array(
-                'criteria'      =>  $criteria,        
-                'pagination'    =>  array(
-                    'pageSize'=>40,
-                ),
-            ));
-            
-            $this->render('index',array(
-                'yuristsDataProvider'   =>  $yuristsDataProvider,
-            ));
-            
-        }
+    /**
+     * Specifies the access control rules.
+     * This method is used by the 'accessControl' filter.
+     * @return array access control rules
+     */
+    public function accessRules() {
+        return array(
+            array('allow', // allow all users 
+                'actions' => array('index'),
+                'users' => array('*'),
+            ),
+            array('deny', // deny all users
+                'users' => array('*'),
+            ),
+        );
+    }
+
+    public function actionIndex() {
+        $criteria = new CDbCriteria;
+
+        $criteria->order = "karma DESC";
+        $criteria->with = array("settings", "town", "town.region", "categories", "answersCount");
+        $criteria->addColumnCondition(array('active100' => 1));
+        $criteria->addColumnCondition(array('avatar!' => ''));
+        $criteria->addCondition("role = " . User::ROLE_JURIST);
+
+        $yuristsDataProvider = new CActiveDataProvider('User', array(
+            'criteria' => $criteria,
+            'pagination' => array(
+                'pageSize' => 40,
+            ),
+        ));
+
+        $this->render('index', array(
+            'yuristsDataProvider' => $yuristsDataProvider,
+        ));
+    }
+
 }
