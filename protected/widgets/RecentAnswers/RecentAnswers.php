54cdb071ed158008a77b6d056fd8f818d9879327
Michael Krutikov <mukrytikov@atomstandard.ru>
2017-09-07 16:29:38 +0300

    кеширование на главной

--- protected/widgets/RecentAnswers/RecentAnswers.php
+++ protected/widgets/RecentAnswers/RecentAnswers.php
@@ -12,17 +12,37 @@ class RecentAnswers extends CWidget
     
     public function run()
     {
-        
+        $dateStart = date('Y-m-d', time()-60*60*24*130);
         // выберем ответы за последний месяц, с вопросами и авторами
-        $answersRows = Yii::app()->db->cache($this->cacheTime)->createCommand()
+//        $answersRows = Yii::app()->db->cache($this->cacheTime)->createCommand()
+//                ->select('q.title questionTitle, q.id questionId, q.price questionPrice, q.payed questionPayed, a.answerText, a.datetime answerTime, a.authorId, u.name authorName, u.name2 authorName2, u.lastName authorLastName, u.lastActivity')
+//                ->from('{{answer}} a')
+//                ->leftJoin('{{user}} u', 'a.authorId = u.id')
+//                ->leftJoin('{{question}} q', 'q.id = a.questionId')
+//                ->where('a.datetime>:dateStart AND a.status!=2 AND a.videoLink="" AND u.active100=1', array(':dateStart' => $dateStart))
+//                ->order('answerTime DESC')
+//                ->queryAll();
+        $answersIds = Yii::app()->db->cache($this->cacheTime)->createCommand()
+                ->select('a.id') 
+                ->from('{{answer}} a')
+                ->leftJoin('{{user}} u', 'a.authorId = u.id')
+                ->where('a.datetime>:dateStart AND a.status!=2 AND a.videoLink="" AND u.active100=1', array(':dateStart' => $dateStart))
+                ->group('a.authorId')
+                ->order('a.datetime DESC')
+                ->limit($this->limit)
+                ->queryColumn();
+        //CustomFuncs::printr($answersIds);
+        
+        $answersRows = Yii::app()->db->createCommand()
                 ->select('q.title questionTitle, q.id questionId, q.price questionPrice, q.payed questionPayed, a.answerText, a.datetime answerTime, a.authorId, u.name authorName, u.name2 authorName2, u.lastName authorLastName, u.lastActivity')
                 ->from('{{answer}} a')
                 ->leftJoin('{{user}} u', 'a.authorId = u.id')
                 ->leftJoin('{{question}} q', 'q.id = a.questionId')
-                ->where('a.datetime>NOW()-INTERVAL 130 DAY AND a.status!=2 AND a.videoLink="" AND u.active100=1')
+                ->where(array('in', 'a.id', $answersIds))
                 ->order('answerTime DESC')
                 ->queryAll();
-        
+        //CustomFuncs::printr($answersRows);
+        //exit;
        
         $answers = array();
         $authorId = 0;
