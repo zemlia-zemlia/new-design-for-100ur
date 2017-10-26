160947ab41abb8e5891e355c0737e3222e3da6d3
Michael Krutikov <mukrytikov@atomstandard.ru>
2017-10-26 14:37:12 +0300

    хеш вместо id транзакции у вебмастера

--- protected/modules/webmaster/views/transaction/_view.php
+++ protected/modules/webmaster/views/transaction/_view.php
@@ -1,5 +1,5 @@
 <tr>
-    <td><?php echo $data->id;?></td>
+    <td><small><?php echo md5($data->id);?></small></td>
     <td>
         <?php echo CustomFuncs::niceDate($data->datetime);?>
         <?php if($data->leadId != 0 && time()-strtotime($data->datetime)<86400*3):?>
