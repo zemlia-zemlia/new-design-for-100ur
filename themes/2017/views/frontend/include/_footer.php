5fed0c2e8184f0e94b2080165eb9771159caabde
Michael <Michael@192.168.1.43>
2017-11-11 10:41:04 +0300

    footer

--- themes/2017/views/frontend/include/_footer.php
+++ themes/2017/views/frontend/include/_footer.php
@@ -82,76 +82,7 @@
                 
                 
             </div>
-            <!--
-            <div class='col-md-3 col-sm-3 center-align'>
-                <img src='/pics/2017/logo_dark.png' alt="100 Юристов и Адвокатов" title="Юридический портал" class='center-block' />
-                <div class="logo-description-footer">
-                    <h5>юридические консультации онлайн</h5>    
-                </div>
-                <div id="social-icons-container">
-                    <noindex>
-                        <a href="https://www.instagram.com/100yuristov/" target="_blank" rel="nofollow"><img src="/pics/2017/ig_icon.png" alt="Instagram" /></a>
-                        <a href="https://vk.com/sto_yuristov" target="_blank" rel="nofollow"><img src="/pics/2017/vk_icon.png" alt="VK" /></a>
-                        <a href="https://ok.ru/group/53087450366125" target="_blank" rel="nofollow"><img src="/pics/2017/ok_icon.png" alt="Одноклассники" /></a>
-                        <a href="https://www.youtube.com/channel/UCgleswVaxaLKwL-MeGDmtfQ" target="_blank" rel="nofollow"><img src="/pics/2017/yt_icon.png" alt="Youtube" /></a>
-                        <a href="https://www.facebook.com/100-%D0%AE%D1%80%D0%B8%D1%81%D1%82%D0%BE%D0%B2-1384104981880799/" target="_blank" rel="nofollow"><img src="/pics/2017/fb_icon.png" alt="Facebook" /></a>
-                        <a href="https://twitter.com/stoyuristov" target="_blank" rel="nofollow"><img src="/pics/2017/tw_icon.png" alt="Twitter" /></a>
-                    </noindex>
-                </div>
-            </div>
-
-            <div class='col-md-3 col-sm-3'>
-                    <ul class="small">
-                        <li><?php echo ($_SERVER['REQUEST_URI'] != '/q/')?CHtml::link('Новые вопросы', Yii::app()->createUrl('/question/index')):'<span class="active">Новые вопросы</span>';?></li> 			
-                        <li><?php echo ($_SERVER['REQUEST_URI'] != '/site/offer/') ? CHtml::link('Пользовательское соглашение', Yii::app()->createUrl('/site/offer/')) : '<span class="active">Пользовательское соглашение</span>'; ?></li>
-                        <li><?php echo ($_SERVER['REQUEST_URI'] != '/site/crm/') ? CHtml::link('CRM Для юридических фирм', Yii::app()->createUrl('/site/crm/')) : '<span class="active">CRM Для юридических фирм</span>'; ?></li>
-                        <li><?php echo ($_SERVER['REQUEST_URI'] != '/site/lead/') ? CHtml::link('Юридические заявки', Yii::app()->createUrl('/site/lead/')) : '<span class="active">Юридические заявки</span>'; ?></li>
-                        <li><?php echo ($_SERVER['REQUEST_URI'] != '/site/klienti_dlya_yuristov/') ? CHtml::link('Клиенты для юристов', Yii::app()->createUrl('/site/klienti_dlya_yuristov/')) : '<span class="active">Клиенты для юристов</span>'; ?></li>
-                        <li><?php echo ($_SERVER['REQUEST_URI'] != '/site/partners/') ? CHtml::link('Партнерская программа', Yii::app()->createUrl('/site/partners/')) : '<span class="active">Партнерская программа</span>'; ?></li>
-                        <li><?php echo ($_SERVER['REQUEST_URI'] != '/site/about/') ? CHtml::link('О проекте', Yii::app()->createUrl('/site/about/')) : '<span class="active">О проекте</span>'; ?></li>
-                        <li><?php echo ($_SERVER['REQUEST_URI'] != '/region/') ? CHtml::link('География', Yii::app()->createUrl('/region/')) : '<span class="active">География</span>'; ?></li>
-                        <li><?php echo ($_SERVER['REQUEST_URI'] != '/company/') ? CHtml::link('Каталог компаний России', Yii::app()->createUrl('/company/')) : '<span class="active">Каталог компаний</span>'; ?></li>
-                    </ul>
-            </div>
-
-            <div class='col-md-3 col-sm-3 small'>
-                <div itemscope itemtype="http://schema.org/Organization"> 
-                    <span itemprop="name">100 Юристов</span>
-                    <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-                        <span itemprop="addressLocality">Москва</span> <span itemprop="streetAddress">Шлюзовая набережная д.6 стр.4</span>
-                    </div>
-                    <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-                        <span itemprop="addressLocality">Санкт-Петербург</span> <span itemprop="streetAddress">Ул. Достоевского д.25</span>
-                    </div>
-                    <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-                        <span itemprop="addressLocality">Нижний Новгород</span> <span itemprop="streetAddress">Ул. Новая, д. 28</span>
-                    </div>
-                    <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-                        <span itemprop="addressLocality">Екатеринбург</span> <span itemprop="streetAddress">Ул. 8 Марта, д. 142</span>
-                    </div>
-                    <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-                        <span itemprop="addressLocality">Ростов-на-Дону</span> <span itemprop="streetAddress">Ул. Красноармейская, д. 142/50</span>
-                    </div>
-                    <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
-                        <span itemprop="addressLocality">Краснодар</span> <span itemprop="streetAddress">Ул. Московская, 148</span>
-                    </div>    
-                    <span itemprop="telephone">8-800-500-61-85</span>
-                    
-                </div>
-            </div>
-
-            <div class='col-md-3 col-sm-3' style="text-align: center">
-                <div class="header-phone">
-                    <strong>Горячая линия</strong> <br/>юридических консультаций
-                </div>
-                <span class="header-phone-800">
-                    <strong>
-                        8-800-500-61-85 
-                    </strong>
-                </span>
-            </div>
-        </div>
-        -->    
+        </div>     
         <div class='row'>
             <div class='col-md-12 col-sm-12'>
                 <p style="text-align: justify;"> 
