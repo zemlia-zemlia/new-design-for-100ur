5bd367606c9b4603f2f00a9cbc2809d5ffbc6f3b
Michael Krutikov <mukrytikov@atomstandard.ru>
2017-11-21 17:00:43 +0300

    пакетная загрузка контактов

---
+++ protected/commands/ExportToSendpulseCommand.php
@@ -0,0 +1,23 @@
+<?php
+use Sendpulse\RestApi\ApiClient;
+use Sendpulse\RestApi\Storage\FileStorage;
+    
+/**
+ * Консольная команда для загрузки активных пользователей в Sendpulse через API
+ */
+class ExportToSendpulseCommand extends CConsoleCommand
+{
+    public function actionIndex()
+    {
+        $criteria = new CDbCriteria();
+        $criteria->addColumnCondition(['active100'=>1]);
+        $criteria->addInCondition('role', [User::ROLE_BUYER, User::ROLE_CLIENT, User::ROLE_JURIST, User::ROLE_PARTNER]);
+        $users = User::model()->findAll($criteria);
+        $usersCount = sizeof($users);
+        
+        foreach($users as $index=>$user) {
+            echo $index . '/' . $usersCount . ': ' . $user->email . PHP_EOL;
+            $user->addToSendpulse();
+        }
+    }
+}
