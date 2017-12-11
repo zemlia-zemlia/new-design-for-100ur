89c7e4e8ab50dfc4f8136008e45acd01586c1894
Michael Krutikov <mukrytikov@atomstandard.ru>
2017-12-11 11:43:43 +0300

    оптимизация выборки последних ответов

--- protected/widgets/RecentAnswers/RecentAnswers.php
+++ protected/widgets/RecentAnswers/RecentAnswers.php
@@ -7,7 +7,7 @@
 class RecentAnswers extends CWidget
 {
     public $template = 'default'; // представление виджета по умолчанию
-    public $cacheTime = 0; // время кеширования
+    public $cacheTime = 600; // время кеширования
     public $limit = 6;
     
     public function run()
@@ -16,31 +16,24 @@ class RecentAnswers extends CWidget
         $answers = Yii::app()->cache->get('recentAnswers');
         
         if($answers === false) {
-            $dateStart = date('Y-m-d', time()-60*60*24*130);
+            
             // выберем ответы за последний месяц, с вопросами и авторами
-            $answersRows = Yii::app()->db->cache($this->cacheTime)->createCommand()
+            $answersRows = Yii::app()->db->createCommand()
                     ->select('q.title questionTitle, q.id questionId, q.price questionPrice, q.payed questionPayed, a.answerText, a.datetime answerTime, a.authorId, u.name authorName, u.name2 authorName2, u.lastName authorLastName, u.lastActivity')
                     ->from('{{answer}} a')
-                    ->leftJoin('{{user}} u', 'a.authorId = u.id')
+                    ->leftJoin('{{user}} u', 'a.authorId = u.id AND u.lastAnswer = a.datetime')
                     ->leftJoin('{{question}} q', 'q.id = a.questionId')
-                    ->where('a.datetime>:dateStart AND a.status!=2 AND a.videoLink="" AND u.active100=1', array(':dateStart' => $dateStart))
-                    ->order('answerTime DESC')
+                    ->where('u.lastAnswer IS NOT NULL AND u.active100=1')
+                    ->order('u.lastAnswer DESC')
+                    ->limit($this->limit)
                     ->queryAll();
 
             $answers = array();
-            $authorId = 0;
-
-            // нам нужны ответы от уникальных авторов, выберем их в массив $answers
+            
             foreach($answersRows as $row) {
-                if($row['authorId']!=$authorId && !$answers[$row['authorId']]) {
-                    $answers[$row['authorId']] = $row;
-                    $authorId = $row['authorId'];
-                }
-                // в выходном массиве нужно не более $limit элементов
-                if(sizeof($answers) == $this->limit) {
-                    break;
-                }
+                $answers[$row['authorId']] = $row;
             }
+            
             Yii::app()->cache->set('recentAnswers', $answers, $this->cacheTime);
         }                    
         $this->render($this->template, array(
