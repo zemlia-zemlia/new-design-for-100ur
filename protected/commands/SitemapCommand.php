51aa0fd8e5dfdea48edabc3c241e340a4a66d518
Михаил Ю. Крутиков <mykrutikov@atomstandart.local>
2018-01-15 14:02:16 +0300

    базовый урл

--- protected/commands/SitemapCommand.php
+++ protected/commands/SitemapCommand.php
@@ -2,10 +2,9 @@
 
 class SitemapCommand extends CConsoleCommand
 {
-    // рассылка уведомлений
     public function actionIndex()
     {
-        $siteUrl = Yii::app()->baseUrl;
+        $siteUrl = Yii::app()->urlManager->baseUrl;
         
         $siteMap = '<?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
