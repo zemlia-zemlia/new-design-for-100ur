6825cb81f8912c92a6db0281bdc26be565299b0c
Michael <Michael@192.168.1.43>
2018-02-17 18:05:50 +0300

    партнерская программа

--- themes/2017/views/frontend/include/_header.php
+++ themes/2017/views/frontend/include/_header.php
@@ -32,6 +32,9 @@
                 });
             </script>
         <?php endif; ?>
+            
+        <meta property="og:image" content="https://100yuristov.com/pics/2017/100_yuristov_logo_blue.jpg">
+        <meta name="twitter:card" content="https://100yuristov.com/pics/2017/100_yuristov_logo_blue.jpg">
     </head>  
 
     <body>
