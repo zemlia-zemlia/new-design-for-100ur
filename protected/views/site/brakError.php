457a4c4de0369dd674b9b058411c256a82aa8b8b
Michael <Michael@192.168.1.43>
2018-02-23 14:12:41 +0300

    сообщения при отбраковке лида

---
+++ protected/views/site/brakError.php
@@ -0,0 +1,13 @@
+<?php
+    $this->setPageTitle("Ошибка отбраковки");
+?>
+
+
+<h1><?php echo $errorTitle;?></h1>
+
+<div class="alert alert-warning">
+
+    <p>
+        <?php echo $errorMessage;?>
+    </p>
+</div>
