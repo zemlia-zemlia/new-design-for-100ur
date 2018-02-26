b88d141ff1a3d7a80c05086417f1ebe092b3aea1
Michael Krutikov <m@mkrutikov.pro>
2018-02-26 18:25:24 +0300

    временно спрятали телефон

--- protected/widgets/Hotline/views/default.php
+++ protected/widgets/Hotline/views/default.php
@@ -4,11 +4,12 @@
     </div>
 
     <div class="phone-wrapper">
-        <!--
+
         <span class="header-phone-800"><strong>
-                8-800-500-61-85</strong>
+                8-800-500-</strong>
+
         </span>
-        &nbsp;-->
-        <?php echo CHtml::link('Заказать звонок', Yii::app()->createUrl('question/call'), array('class'=>'button button-green-border')); ?>
+        <?php echo CHtml::link('Заказать звонок', Yii::app()->createUrl('question/call'), array('class' => 'button button-green-border')); ?>
+
     </div>
 </div>
