95990475ff50c410631e3ae3f1baad0b535aa784
Michael <Michael@192.168.1.43>
2018-04-29 11:26:39 +0300

    добавил параметр period для парсеров почты

--- protected/commands/ParseMail9111Command.php
+++ protected/commands/ParseMail9111Command.php
@@ -5,8 +5,15 @@
  */
 class ParseMail9111Command extends CConsoleCommand {
 
-    public function actionIndex($debugMode = false) {
+    /**
+     * Запуск парсинга писем
+     * Параметры указываются при запуске скрипта, например:
+     * ./yiic ParseMail9111 index --debugMode=false --period=4
+     * @param type $debugMode включен ли дебаг
+     * @param type $period период в сутках, за который парсим лиды
+     */
+    public function actionIndex($debugMode = false, $period = 2) {
         $parser = EmailParserFactory::getParser('admin_100yuristov', '9111');
-        $parser->run($debugMode);
+        $parser->run($debugMode, $period);
     }
 }
--- protected/components/EmailParser.php
+++ protected/components/EmailParser.php
@@ -46,10 +46,12 @@ abstract class EmailParser {
 
     /**
      * Извлекаем массив текстов непрочитанных писем из указанной папки
-     * @param type $folderName
+     * Письма берутся не старше чем $period дней
+     * @param string $folderName имя папки
+     * @param integer $period за сколько последних дней собирать письма
      * @return array Массив объектов ParsedEmail
      */
-    protected function getMessagesFromFolder($folderName) {
+    protected function getMessagesFromFolder($folderName, $period = 2) {
         $host = $this->_accessConfig['server'];
         $port = $this->_accessConfig['port'];
         $login = $this->_accessConfig['login'];
@@ -65,7 +67,7 @@ abstract class EmailParser {
         };
 
         // извлекаем письма из папки в ящике
-        $emails = imap_search($mbox, 'UNSEEN SINCE ' . date('d-M-Y', strtotime("-1 day")));
+        $emails = imap_search($mbox, 'UNSEEN SINCE ' . date('d-M-Y', strtotime("-" . $period . " day")));
         if ($emails == false && imap_errors()) {
             throw new CException("Messages search wrong criteria");
         }
@@ -113,12 +115,12 @@ abstract class EmailParser {
 
     /**
      * Поиск в базе телефонов существующих лидов
-     * @param integer $period За сколько часов собирать лиды
+     * @param integer $period За сколько суток собирать лиды
      */
-    protected function setExistingPhones($period = 48) {
+    protected function setExistingPhones($period = 2) {
 
         $existingLeads = Lead100::model()->findAll(array(
-            'condition' => 'question_date>NOW()- INTERVAL ' . $period . ' HOUR AND sourceId IN(' . implode(', ', $this->_leadSourceIds) . ')',
+            'condition' => 'question_date>NOW()- INTERVAL ' . $period . ' DAY AND sourceId IN(' . implode(', ', $this->_leadSourceIds) . ')',
         ));
 
         // массив, в котором будут храниться телефоны лидов, которые добавлены в базу за последний день, чтобы не добавить одного лида несколько раз
@@ -144,16 +146,17 @@ abstract class EmailParser {
     /**
      * Метод, вызывающий цепочку методов парсинга, является фасадом для клиентского кода
      * @param boolean $debugMode Работает ли скрипт в режиме отладки
+     * @param type $period период в сутках, за который парсим лиды
      */
-    public function run($debugMode = false) {
+    public function run($debugMode = false, $period = 2) {
         
         $this->setLeadSourcesIds();
-        $this->setExistingPhones();
+        $this->setExistingPhones($period);
         $this->_debugMode = $debugMode;
 
         foreach ($this->_folderSettings as $folderName => $folderSettings) {
             $this->echoDebug($folderName);
-            $this->_messages = $this->getMessagesFromFolder($folderName);
+            $this->_messages = $this->getMessagesFromFolder($folderName, $period);
 
             foreach ($this->_messages as $message) {
                 $this->parseMessage($message, new Lead100, $folderSettings);
