11b9364f61bf3c08509c386471a7182ee10ec56f
Michael Krutikov <m.krutikov@4slovo.ru>
2018-06-04 17:56:08 +0300

    константы вынесены из индекса в конфиг

--- index.php
+++ index.php
@@ -2,14 +2,11 @@
 
 // change the following paths if necessary
 $composer = dirname(__FILE__) . '/protected/vendor/autoload.php';
-$yii=dirname(__FILE__).'/protected/vendor/yiisoft/yii/framework/yii.php';
+$yii=dirname(__FILE__).'../../framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
-// remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG',true);
-// режим разработки
-defined('YII_DEV') or define('YII_DEV', true);
-// specify how many levels of call stack should be shown in each log message
-defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+$settings=dirname(__FILE__).'/protected/config/settings.php';
+
+require_once($settings);
 require_once($composer);
 require_once($yii);
 Yii::createWebApplication($config)->run();
