93abe2620ebb1bfcfde3ce9cfec3f15fb60493aa
Michael <Michael@192.168.1.43>
2018-06-23 17:52:35 +0300

    bugfix

--- protected/components/apiClasses/ApiLexprofit.php
+++ protected/components/apiClasses/ApiLexprofit.php
@@ -43,7 +43,7 @@ class ApiLexprofit implements ApiClassInterface
     
     private function checkResponse($apiResponse, $lead)
     {
-        if($apiResponse['success']) {
+        if(is_array($apiResponse) && $apiResponse['success']) {
             LoggerFactory::getLogger()->log('Лид #' . $lead->id . ' отправлен в партнерку Lexprofit', 'Lead', $lead->id);
             return true;
         } else {
