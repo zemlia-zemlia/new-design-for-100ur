be9a76647d68f05fde938a885a922eba9b6bf79b
Michael <Michael@192.168.1.43>
2018-06-28 18:31:47 +0300

    улучшена читаемость лога

--- protected/widgets/LogReader/views/default.php
+++ protected/widgets/LogReader/views/default.php
@@ -3,7 +3,7 @@
         <?php foreach ($records as $record): ?>
             <tr>
                 <td></td>
-                <td><?php echo $record['message']; ?></td>
+                <td><?php echo str_replace(':', ': ', $record['message']); ?></td>
                 <td><?php echo CustomFuncs::niceDate($record['created'], true, false); ?></td>
             </tr>
         <?php endforeach; ?>
