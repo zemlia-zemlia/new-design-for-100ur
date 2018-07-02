ecd155391e3c017c171e1ba58a42e4ba02dc9023
Michael <Michael@192.168.1.43>
2018-07-02 12:49:52 +0300

    вывод ссылок в логе

--- protected/extensions/Logger/LogReader.php
+++ protected/extensions/Logger/LogReader.php
@@ -24,5 +24,30 @@ class LogReader
         
         return $rows;
     }
+    
+    /**
+     * Создает ссылку на сущность, привязанную к элементу лога
+     * @param type $logRow
+     */
+    public static function createLink($logRow)
+    {
+        if($logRow['class'] == '' || $logRow['subjectId'] == 0) {
+            return '';
+        }
+        
+        switch($logRow['class']) {
+            case 'Lead':
+                return CHtml::link('Лид #' . $logRow['subjectId'], Yii::app()->createUrl('admin/lead/view', ['id' => $logRow['subjectId']]));
+            
+            case 'User':
+                return CHtml::link('Польз. #' . $logRow['subjectId'], Yii::app()->createUrl('admin/user/view', ['id' => $logRow['subjectId']]));
+            
+            case 'Question':
+                return CHtml::link('Вопрос. #' . $logRow['subjectId'], Yii::app()->createUrl('admin/question/view', ['id' => $logRow['subjectId']]));
+            
+            default:
+                return '';
+        }
+    }
 }
 
--- protected/widgets/LogReader/views/default.php
+++ protected/widgets/LogReader/views/default.php
@@ -2,8 +2,8 @@
     <table class="table table-bordered">
         <?php foreach ($records as $record): ?>
             <tr>
-                <td></td>
-                <td><?php echo str_replace(':', ': ', $record['message']); ?></td>
+                <td><?php echo LogReader::createLink($record);?></td>
+                <td><?php echo CHtml::encode(str_replace(':', ': ', $record['message'])); ?></td>
                 <td><?php echo CustomFuncs::niceDate($record['created'], true, false); ?></td>
             </tr>
         <?php endforeach; ?>
