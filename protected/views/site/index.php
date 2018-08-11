453fec84ade711b903f7b2e08be2542ed6b26e07
Michael <Michael@192.168.1.43>
2018-08-11 13:39:03 +0300

    seo правки главной

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,6 +1,7 @@
 <?php
 $this->setPageTitle("Бесплатная юридическая консультация онлайн и по телефону круглосуточно" . Yii::app()->name);
 Yii::app()->clientScript->registerMetaTag("На нашем сайте вы можете получить бесплатную правовую помощь без регистрации.  Вы можете задать любой вопрос юристу или самостоятельно найти ответ в нашей правовой базе.", 'description');
+Yii::app()->clientScript->registerMetaTag("Бесплатная юридическая консультация онлайн", 'keywords');
 Yii::app()->clientScript->registerLinkTag("canonical", NULL, "https://" . $_SERVER['SERVER_NAME']);
 ?>
 <h1>Бесплатная юридическая консультация</h1>
