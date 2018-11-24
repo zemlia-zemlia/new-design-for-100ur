3801a636e5cbbe802d0749ef42a7f20d6f7b60a2
Michael <misha.sunsetboy@gmail.com>
2018-11-24 10:58:54 +0300

    в админке на странице категории улучшены хлебные крошки

--- protected/modules/admin/views/questionCategory/view.php
+++ protected/modules/admin/views/questionCategory/view.php
@@ -2,51 +2,54 @@
 /* @var $this QuestionCategoryController */
 /* @var $model QuestionCategory */
 
-$this->setPageTitle(CHtml::encode($model->id) . ". Категории вопросов. ". Yii::app()->name);
+$this->setPageTitle(CHtml::encode($model->id) . ". Категории вопросов. " . Yii::app()->name);
 
-$this->breadcrumbs=array(
-        'Вопросы и ответы'=>array('/admin/question'),
-	'Категории вопросов'=>array('index'),
-	CHtml::encode($model->name),
-);
+$this->breadcrumbs = [
+    'Категории вопросов' => array('index'),
+];
+
+$ancestors = $model->ancestors()->findAll();
+foreach ($ancestors as $ancestor) {
+    $this->breadcrumbs[$ancestor->name] = Yii::app()->createUrl('questionCategory/alias', $ancestor->getUrl());
+}
+$this->breadcrumbs[] = CHtml::encode($model->name);
 
 $this->widget('zii.widgets.CBreadcrumbs', array(
-    'homeLink'=>CHtml::link('100 юристов',"/admin/"),
-    'separator'=>' / ',
-    'links'=>$this->breadcrumbs,
- ));
+    'homeLink' => CHtml::link('100 юристов', "/admin/"),
+    'separator' => ' / ',
+    'links' => $this->breadcrumbs,
+));
 ?>
 
 
 <div class="row">
-	<div class="col-md-9 small">
-	<br/>
-	<h1 class="vert-margin30"><?php echo CHtml::encode($model->name);?></h1>
-	<br/>
-	<?php if ($model->description1): ?>
-		<div class="vert-margin30">
-			<?php echo $model->description1; ?>
-		</div>
-	<?php endif; ?>
-	
-	<h3>Вложенные категории:</h3>
+    <div class="col-md-9 small">
+        <br/>
+        <h1 class="vert-margin30"><?php echo CHtml::encode($model->name); ?></h1>
+        <br/>
+        <?php if ($model->description1): ?>
+            <div class="vert-margin30">
+                <?php echo $model->description1; ?>
+            </div>
+        <?php endif; ?>
 
-<?php $this->renderPartial('_table', ['categoriesArray' => $subCategoriesArray]);?>
-	</div>
+        <h3>Вложенные категории:</h3>
 
-
-<div class="col-md-3">
-    <?php echo CHtml::link('Редактировать категорию', Yii::app()->createUrl('/admin/questionCategory/update', array('id'=>$model->id)), array('class'=>'btn btn-block btn-primary'));?>
-	<?php echo CHtml::link('Открыть на сайте', Yii::app()->createUrl('/questionCategory/alias', array('name'=>$model->alias)), array('class'=>'btn btn-block btn-default', 'target' => '_blank'));?>
-	<br/>
-	<p>Заглавная картинка категории:</p>
-	<?php if($model->image):?>
-		<?php echo CHtml::image($model->getImagePath(), '', ['class' => 'img-responsive']);?>
-	<?php endif;?>
-	
+        <?php $this->renderPartial('_table', ['categoriesArray' => $subCategoriesArray]); ?>
+    </div>
 
 
-</div>
+    <div class="col-md-3">
+        <?php echo CHtml::link('Редактировать категорию', Yii::app()->createUrl('/admin/questionCategory/update', array('id' => $model->id)), array('class' => 'btn btn-block btn-primary')); ?>
+        <?php echo CHtml::link('Открыть на сайте', Yii::app()->createUrl('/questionCategory/alias', array('name' => $model->alias)), array('class' => 'btn btn-block btn-default', 'target' => '_blank')); ?>
+        <br/>
+        <p>Заглавная картинка категории:</p>
+        <?php if ($model->image): ?>
+            <?php echo CHtml::image($model->getImagePath(), '', ['class' => 'img-responsive']); ?>
+        <?php endif; ?>
+
+
+    </div>
 
 </div>
 
