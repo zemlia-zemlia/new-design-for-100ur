f587259f3535ce2abf362d91e6eb35b93c9b471f
Michael <misha.sunsetboy@gmail.com>
2018-12-08 12:27:51 +0300

    раздел категорий, правки

--- protected/modules/admin/controllers/QuestionCategoryController.php
+++ protected/modules/admin/controllers/QuestionCategoryController.php
@@ -169,11 +169,14 @@ class QuestionCategoryController extends Controller
                 $attachmentFile->filename = $attachmentFile->createFileName($attachment);
                 $attachmentFileFolder = $attachmentFile->createFolderFromFileName();
 
-                if (@mkdir(Yii::getPathOfAlias('webroot') . $attachmentFileFolder, 0777, true)) {
-                    if ($attachment->saveAs(Yii::getPathOfAlias('webroot') . $attachmentFileFolder . '/' . $attachmentFile->filename)) {
-                        $attachmentFile->save();
-                    }
+                if(!is_dir(Yii::getPathOfAlias('webroot') . $attachmentFileFolder)) {
+                    @mkdir(Yii::getPathOfAlias('webroot') . $attachmentFileFolder, 0777, true);
                 }
+
+                if ($attachment->saveAs(Yii::getPathOfAlias('webroot') . $attachmentFileFolder . '/' . $attachmentFile->filename)) {
+                    $attachmentFile->save();
+                }
+
 
             }
 
