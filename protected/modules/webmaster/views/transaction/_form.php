b71934415a0331d68ada87f839b1ff406ca4a1ac
Michael <misha.sunsetboy@gmail.com>
2019-07-06 13:19:46 +0300

    minor fix 7.2

--- protected/modules/webmaster/views/transaction/_form.php
+++ protected/modules/webmaster/views/transaction/_form.php
@@ -1,29 +1,26 @@
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'transaction-form',
-	'enableAjaxValidation'=>false,
-)); ?>
-
-    <?php// echo $form->errorSummary($model, 'Исправьте ошибки'); ?>
-
+    <?php $form = $this->beginWidget('CActiveForm', array(
+        'id' => 'transaction-form',
+        'enableAjaxValidation' => false,
+    )); ?>
     <div class="row">
         <div class="col-sm-9">
             <div class="form-group">
-                <?php echo $form->textField($model,'comment',array('class'=>'form-control right-align', 'placeholder' => 'Номер кошелька Яндекс Деньги')); ?>
-                <?php echo $form->error($model,'comment'); ?>
-            </div>  
+                <?php echo $form->textField($model, 'comment', array('class' => 'form-control right-align', 'placeholder' => 'Номер кошелька Яндекс Деньги')); ?>
+                <?php echo $form->error($model, 'comment'); ?>
+            </div>
         </div>
         <div class="col-sm-3">
             <div class="form-group">
-            <?php echo $form->textField($model,'sum',array('class'=>'form-control right-align', 'placeholder' => 'Сумма')); ?>
-            <?php echo $form->error($model,'sum'); ?>
-            </div>       
+                <?php echo $form->textField($model, 'sum', array('class' => 'form-control right-align', 'placeholder' => 'Сумма')); ?>
+                <?php echo $form->error($model, 'sum'); ?>
+            </div>
         </div>
     </div>
-    
-    <?php echo CHtml::submitButton('Вывести средства', array('class'=>'btn btn-primary btn-block')); ?>
-    
-<?php $this->endWidget(); ?>
+
+    <?php echo CHtml::submitButton('Вывести средства', array('class' => 'btn btn-primary btn-block')); ?>
+
+    <?php $this->endWidget(); ?>
 
 </div><!-- form -->
