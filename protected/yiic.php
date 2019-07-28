ea6d95b70322dda11d4d4ae6ce33e3b50173cd4b
Michael <misha.sunsetboy@gmail.com>
2019-07-28 22:20:09 +0300

    фреймворк теперь как композер пакет

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,7 @@
 <?php
 $composer = dirname(__FILE__) . '/vendor/autoload.php';
 // change the following paths if necessary
-$yiic=dirname(__FILE__).'/../../../framework/yiic.php';
+$yiic=dirname(__FILE__).'/vendor/yiisoft/yii/framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
 $settings=dirname(__FILE__).'/config/settings.php';
 
