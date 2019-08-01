a8eef57a185c530305045e749684f3cefb9ee443
Michael <misha.sunsetboy@gmail.com>
2019-08-01 08:31:40 +0300

    попытки настроить тесты

--- protected/tests/_bootstrap.php
+++ protected/tests/_bootstrap.php
@@ -1,2 +1,11 @@
 <?php
-// This is global bootstrap for autoloading
+// change the following paths if necessary
+$composer = dirname(__FILE__) . '/../vendor/autoload.php';
+$yiit = dirname(__FILE__).'/../vendor/yiisoft/yii/framework/yiit.php';
+$config = dirname(__FILE__).'/../config/test.php';
+
+require_once($composer);
+require_once($yiit);
+require_once(dirname(__FILE__).'/WebTestCase.php');
+
+Yii::createWebApplication($config);
