8db1752ebcd77a25f96e260d6b474446fa3caae8
Michael <misha.sunsetboy@gmail.com>
2019-08-01 07:35:03 +0300

    фикс цен лидов в регионах

---
+++ protected/migrations/20190801041917_fix_region_prices.php
@@ -0,0 +1,22 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+/**
+ * Миграция переводит цены лидов региона из рублей в копейки
+ * Class FixRegionPrices
+ */
+class FixRegionPrices extends AbstractMigration
+{
+    const TABLE = '100_region';
+
+    public function up()
+    {
+        $this->query("UPDATE `" . self::TABLE . "` SET sellPrice = sellPrice*100, buyPrice = buyPrice*100");
+    }
+
+    public function down()
+    {
+        $this->query("UPDATE `" . self::TABLE . "` SET sellPrice = sellPrice/100, buyPrice = buyPrice/100");
+    }
+}
