156c5b564667268a753109847604b1a49fc6d898
Mikhail Krutikov <m.krutikov@4slovo.ru>
2019-08-13 13:37:17 +0300

    после оплаты вопроса возвращаемся с меткой

--- protected/views/transaction/_yandexFormQuestion.php
+++ protected/views/transaction/_yandexFormQuestion.php
@@ -1,9 +1,16 @@
+<?php
+/**
+ * @var integer $questionId
+ * @var float $questionPrice
+ */
+?>
+
 <form method="POST" action="https://money.yandex.ru/quickpay/confirm.xml" class="balance-form">
     <input type="hidden" name="receiver" value="410012948838662">
     <input type="hidden" name="label" value="q-<?php echo $questionId; ?>">
     <input type="hidden" name="quickpay-form" value="shop">
     <input type="hidden" name="successURL"
-           value="<?php echo Yii::app()->createUrl('question/view', ['id' => $questionId]); ?>">
+           value="<?php echo Yii::app()->createUrl('question/view', ['id' => $questionId]); ?>?payed_ok=1">
     <input type="hidden" name="targets" value="Оплата вопроса <?php echo $questionId; ?>">
     <input type="hidden" name="paymentType" value="AC">
     <input type="hidden" name="sum" value="<?php echo $questionPrice; ?>" data-type="number"
