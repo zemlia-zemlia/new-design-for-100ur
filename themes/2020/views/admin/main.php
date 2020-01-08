01a473e9389e3354bdce9f0a9ae35f7931b82e4b
andrey <andrey@spb-abris.ru>
2020-01-08 16:54:33 +0300

    all work

--- themes/2020/views/admin/main.php
+++ themes/2020/views/admin/main.php
@@ -72,7 +72,7 @@
         <div class="pull-right hidden-xs">
           <b>Version</b> 2.0
         </div>
-        <strong>Copyright &copy; 2014-2015 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights reserved.
+        <strong>Copyright &copy; 2014-2020 <a href="http://almsaeedstudio.com">100 Юристов</a>.</strong> All rights reserved.
       </footer>
       
       <!-- Control Sidebar -->      
@@ -84,42 +84,42 @@
 
     <!-- jQuery 2.1.4 -->
 <!--    <script src="/adminlte/plugins/jQuery/jQuery-2.1.4.min.js"></script>-->
-<!--    <!-- jQuery UI 1.11.2 -->-->
+<!--    <!-- jQuery UI 1.11.2 -->
 <!--    <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.min.js" type="text/javascript"></script>-->
-<!--    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->-->
+<!--    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 <!--    <script>-->
 <!--      $.widget.bridge('uibutton', $.ui.button);-->
 <!--    </script>-->
-<!--    <!-- Bootstrap 3.3.2 JS -->-->
+<!--    <!-- Bootstrap 3.3.2 JS -->
 <!--    <script src="/adminlte/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>-->
-<!--    <!-- Morris.js charts -->-->
+<!--    <!-- Morris.js charts -->
 <!--    <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>-->
 <!--    <script src="/adminlte/plugins/morris/morris.min.js" type="text/javascript"></script>-->
-<!--    <!-- Sparkline -->-->
+<!--    <!-- Sparkline -->
 <!--    <script src="/adminlte/plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>-->
-<!--    <!-- jvectormap -->-->
+<!--    <!-- jvectormap -->
 <!--    <script src="/adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js" type="text/javascript"></script>-->
 <!--    <script src="/adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>-->
-<!--    <!-- jQuery Knob Chart -->-->
+<!--    <!-- jQuery Knob Chart -->
 <!--    <script src="/adminlte/plugins/knob/jquery.knob.js" type="text/javascript"></script>-->
-<!--    <!-- daterangepicker -->-->
+<!--    <!-- daterangepicker -->
 <!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js" type="text/javascript"></script>-->
 <!--    <script src="/adminlte/plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>-->
-<!--    <!-- datepicker -->-->
+<!--    <!-- datepicker -->
 <!--    <script src="/adminlte/plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>-->
-<!--    <!-- Bootstrap WYSIHTML5 -->-->
+<!--    <!-- Bootstrap WYSIHTML5 -->
 <!--    <script src="/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js" type="text/javascript"></script>-->
-<!--    <!-- Slimscroll -->-->
+<!--    <!-- Slimscroll -->
 <!--    <script src="/adminlte/plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>-->
-<!--    <!-- FastClick -->-->
+<!--    <!-- FastClick -->
 <!--    <script src='/adminlte/plugins/fastclick/fastclick.min.js'></script>-->
-<!--    <!-- AdminLTE App -->-->
+<!--    <!-- AdminLTE App -->
 <!--    <script src="/adminlte/dist/js/app.min.js" type="text/javascript"></script>-->
 <!--    -->
-<!--    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->-->
+<!--    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <!--    <script src="/adminlte/dist/js/pages/dashboard.js" type="text/javascript"></script>-->
 <!--    -->
-<!--    <!-- AdminLTE for demo purposes -->-->
+<!--    <!-- AdminLTE for demo purposes -->
 <!--    <script src="/adminlte/dist/js/demo.js" type="text/javascript"></script>-->
   </body>
 </html>
