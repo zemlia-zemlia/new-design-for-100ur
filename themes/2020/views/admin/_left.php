2321544e5cffb3bfb08c2d563f459480521e1a2e
Vitalii <89269643535@mail.ru>
2020-02-20 19:09:12 +0300

    fix

--- themes/2020/views/admin/_left.php
+++ themes/2020/views/admin/_left.php
@@ -56,7 +56,7 @@
 
                     <li>
                         <a href="<?= Yii::app()->createUrl('/admin/lead/stats') ?>">
-                            <i class="fa fa-th"></i> <span>Статистика</span>
+                            <i class="fa fa-area-chart" aria-hidden="true"></i><span>Статистика</span>
                         </a>
                     </li>
                     <li>
@@ -70,7 +70,7 @@
 
                 <li>
                     <a href="<?= Yii::app()->createUrl('/admin/user/index') ?>">
-                        <i class="fa fa-th"></i> <span>Пользователи</span>
+                        <i class="fa fa-user" aria-hidden="true"></i><span>Пользователи</span>
                     </a>
                 </li>
 
@@ -78,7 +78,7 @@
 
                     <li>
                         <a href="<?= Yii::app()->createUrl('/admin/money') ?>">
-                            <i class="fa fa-th"></i> <span>Касса</span>
+                            <i class="fa fa-money" aria-hidden="true"></i><span>Касса</span>
                         </a>
                     </li>
 
@@ -263,11 +263,10 @@
 
                     <li class="treeview">
                         <a href="#">
-                            <i class="fa fa-table"></i> <span>Новости</span>
-                            <i class="fa fa-angle-left pull-right"></i>
+                            <i class="fa fa-newspaper-o" aria-hidden="true"></i><span>Новости</span>
                         </a>
                         <ul class="treeview-menu">
-                            <li><?php echo CHtml::link("<span class='glyphicon glyphicon-pencil'></span>  Все новости", Yii::app()->createUrl('/admin/blog')); ?></li>
+                            <li><?php echo CHtml::link("<i class=\"fa fa-newspaper-o\" aria-hidden=\"true\"></i>  Все новости", Yii::app()->createUrl('/admin/blog')); ?></li>
                             <li>
                                 <a href="<?= Yii::app()->createUrl('/admin/comment/index', array('type' => Comment::TYPE_POST, 'status' => Comment::STATUS_NEW)) ?>"><i
                                             class="fa fa-circle-o"></i> Новые комментарии
@@ -276,10 +275,10 @@
                             </li>
                             <li>
                             <li>
-                                <?php echo CHtml::link("<span class='glyphicon glyphicon-ok'></span> " . 'Одобренные комментарии', Yii::app()->createUrl('/admin/comment/index', array('type' => Comment::TYPE_POST, 'status' => Comment::STATUS_CHECKED))); ?>
+                                <?php echo CHtml::link("<i class=\"fa fa-comments\" aria-hidden=\"true\"></i> " . 'Одобренные комментарии', Yii::app()->createUrl('/admin/comment/index', array('type' => Comment::TYPE_POST, 'status' => Comment::STATUS_CHECKED))); ?>
                             </li>
                             <li>
-                                <?php echo CHtml::link("<span class='glyphicon glyphicon-fire'></span> " . 'Спам комментарии', Yii::app()->createUrl('/admin/comment/index', array('type' => Comment::TYPE_POST, 'status' => Comment::STATUS_SPAM))); ?>
+                                <?php echo CHtml::link("<i class=\"fa fa-comments\" aria-hidden=\"true\"></i> " . 'Спам комментарии', Yii::app()->createUrl('/admin/comment/index', array('type' => Comment::TYPE_POST, 'status' => Comment::STATUS_SPAM))); ?>
                             </li>
                         </ul>
                     </li>
@@ -295,7 +294,7 @@
                     </li>
                     <li>
                         <a href="<?= Yii::app()->createUrl('/admin/mail/create') ?>">
-                            <i class="fa fa-th"></i> <span>Рассылки</span>
+                            <i class="fa fa-at" aria-hidden="true"></i><span>Рассылки</span>
                         </a>
                     </li>
                 <?php endif; ?>
