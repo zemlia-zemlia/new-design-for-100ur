bd95519028842800b201251270ab46c0ef14ac1a
Michael <misha.sunsetboy@gmail.com>
2020-02-22 17:39:19 +0300

    рефакторинг и покрытие тестами вывода денег юристу

--- protected/modules/admin/controllers/CampaignTransactionController.php
+++ protected/modules/admin/controllers/CampaignTransactionController.php
@@ -70,7 +70,7 @@ class CampaignTransactionController extends Controller
         $criteria->order = 'id DESC';
         $criteria->addCondition('sum<0');
         $criteria->addCondition(['type =' . TransactionCampaign::TYPE_JURIST_MONEYOUT]);
-        
+
         $dataProvider = new CActiveDataProvider('TransactionCampaign', [
             'criteria' => $criteria,
         ]);
@@ -84,18 +84,17 @@ class CampaignTransactionController extends Controller
 
     /**
      * изменение статуса заявки через AJAX
-     * @todo покрыть тестами
      */
     public function actionChange()
     {
-        $requestId = (isset($_POST['id'])) ? (int) $_POST['id'] : false;
-        $requestVerified = (isset($_POST['status'])) ? (int) $_POST['status'] : false;
+        $requestId = (isset($_POST['id'])) ? (int)$_POST['id'] : false;
+        $requestVerified = (isset($_POST['status'])) ? (int)$_POST['status'] : false;
+        $accountId = (int)Yii::app()->request->getParam('accountId');
 
         if (!$requestId || !$requestVerified) {
             echo json_encode(['code' => 400, 'message' => 'Wrong data']);
             Yii::app()->end();
         }
-
 
         $request = TransactionCampaign::model()->findByPk($requestId);
 
@@ -104,56 +103,27 @@ class CampaignTransactionController extends Controller
             Yii::app()->end();
         }
 
-        // обновляем запрос на вывод средств
-        $request->status = $requestVerified;
-
-        $trans = Yii::app()->db->beginTransaction();
-        $transactionCampSave = $request->save();
-
-        if ($transactionCampSave) {
-
-            if ($requestVerified == TransactionCampaign::STATUS_COMPLETE) {
-
-                // меняем баланс пользователя
-                $userBalanceSave = Yii::app()->db->createCommand("UPDATE {{user}} SET balance = balance-" . abs($request->sum) . " WHERE id=" . $request->buyerId)->query();
-
-                
-                // если одобрили вывод средств, создаем транзакцию в кассе
-                $moneyTransaction = new Money();
-                $moneyTransaction->type = Money::TYPE_EXPENCE;
-                $moneyTransaction->direction = 9; // выплаты юристам
-                $moneyTransaction->accountId = (int) $_POST['accountId'];
-                $moneyTransaction->value = abs($request->sum);
-                $moneyTransaction->datetime = date('Y-m-d H:i:s');
-                $moneyTransaction->comment = "Выплата юристу id " . $request->buyerId;
-                $moneyTransactionSave = $moneyTransaction->save();
-
-                if ($userBalanceSave && $moneyTransactionSave){
-                    $trans->commit();
-                    echo json_encode(['code' => 0, 'id' => $request->id, 'message' => 'OK']);
-                    Yii::app()->end();
-                }
-            }
+        if ($request->approveRequest($accountId)) {
+            echo json_encode(['code' => 0, 'id' => $request->id, 'message' => 'OK']);
+        } else {
+            echo json_encode(['code' => 500, 'message' => 'Could not save request' . print_r($request->errors)]);
         }
-
-        $trans->rollback();
-        echo json_encode(['code' => 500, 'message' => 'Could not save request' . print_r($request->errors)]);
-        Yii::app()->end();
     }
 
     /**
      * Returns the data model based on the primary key given in the GET variable.
      * If the data model is not found, an HTTP exception will be raised.
      * @param integer $id the ID of the model to be loaded
-     * @return UserStatusRequest the loaded model
+     * @return TransactionCampaign the loaded model
      * @throws CHttpException
      */
-    public function loadModel($id)
+    public function loadModel($id): TransactionCampaign
     {
         $model = TransactionCampaign::model()->findByPk($id);
         if ($model === null) {
             throw new CHttpException(404, 'The requested page does not exist.');
         }
+
         return $model;
     }
 
---
+++ protected/tests/integration/models/TransactionCampaignTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Tests\integration\models;
+
+use Tests\Factories\CampaignFactory;
+use Tests\Factories\LeadSourceFactory;
+use Tests\Factories\UserFactory;
+use Tests\integration\BaseIntegrationTest;
+use TransactionCampaign;
+use User;
+use Yii;
+
+class TransactionCampaignTest extends BaseIntegrationTest
+{
+    const MONEY_TABLE = '100_money';
+    const USER_TABLE = '100_user';
+    const CAMPAIGN_TRANSACTIONS_TABLE = '100_transactionCampaign';
+
+    protected function _before()
+    {
+        Yii::app()->db->createCommand()->truncateTable(self::USER_TABLE);
+        Yii::app()->db->createCommand()->truncateTable(self::MONEY_TABLE);
+        Yii::app()->db->createCommand()->truncateTable(self::CAMPAIGN_TRANSACTIONS_TABLE);
+    }
+
+    private function loadFixtures(): void
+    {
+        $usersFixture = [
+            (new UserFactory())->generateOne([
+                'name' => 'Юрист при деньгах',
+                'role' => User::ROLE_JURIST,
+                'balance' => 100000,
+                'id' => 10004,
+            ]),
+            (new UserFactory())->generateOne([
+                'name' => 'Юрист без денег',
+                'role' => User::ROLE_JURIST,
+                'balance' => 1000,
+                'id' => 10005,
+            ]),
+        ];
+
+        $this->loadToDatabase(self::USER_TABLE, $usersFixture);
+    }
+
+
+    /**
+     * @dataProvider providerApproveRequest
+     * @param int $userId
+     * @param int $requestAmount
+     * @param int $accountId
+     * @param bool $expectedResult
+     * @param int $expectedBalance
+     * @throws \CException
+     */
+    public function testApproveRequest($userId, $requestAmount, $accountId, $expectedResult, $expectedBalance)
+    {
+        $this->loadFixtures();
+
+        $transaction = new TransactionCampaign();
+        $transaction->buyerId = $userId;
+        $transaction->sum = $requestAmount;
+        $transaction->status = TransactionCampaign::STATUS_PENDING;
+        $transaction->description = 'some description';
+
+        $approveRequestResult = $transaction->approveRequest($accountId);
+
+        $this->assertEquals($expectedResult, $approveRequestResult);
+        if ($expectedResult == true) {
+            $this->tester->seeInDatabase(self::MONEY_TABLE, ['value' => $transaction->sum]);
+        }
+        $this->tester->seeInDatabase(self::USER_TABLE, ['balance' => $expectedBalance]);
+    }
+
+    /**
+     * @return array
+     */
+    public function providerApproveRequest(): array
+    {
+        return [
+            'rich yurist, success' => [
+                'userId' => 10004,
+                'requestAmount' => 50000,
+                'accountId' => 9,
+                'expectedResult' => true,
+                'expectedBalance' => 50000,
+            ],
+            'rich yurist, full withdrawal' => [
+                'userId' => 10004,
+                'requestAmount' => 100000,
+                'accountId' => 9,
+                'expectedResult' => true,
+                'expectedBalance' => 0,
+            ],
+            'poor yurist' => [
+                'userId' => 10005,
+                'requestAmount' => 100000,
+                'accountId' => 9,
+                'expectedResult' => false,
+                'expectedBalance' => 1000,
+            ],
+        ];
+    }
+}
