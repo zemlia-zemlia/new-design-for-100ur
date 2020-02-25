60adad88095c9adab7d38ee17e26159a07114997
Vitalii <89269643535@mail.ru>
2020-02-25 18:00:21 +0300

    верстка страницы мои кампании у баера

--- themes/2020/views/lk/main.php
+++ themes/2020/views/lk/main.php
@@ -87,54 +87,11 @@
       <footer class="main-footer">
         <div class="pull-right hidden-xs">
         </div>
-        <strong>Copyright &copy; 2014-2020 <a href="http://almsaeedstudio.com">100 Юристов</a>.</strong>
+        <strong>&copy; 2014-2020 <a href="#">100 Юристов</a>.</strong>
       </footer>
-      
-      <!-- Control Sidebar -->      
 
-      <!-- Add the sidebar's background. This div must be placed
-           immediately after the control sidebar -->
       <div class='control-sidebar-bg'></div>
     </div><!-- ./wrapper -->
 
-    <!-- jQuery 2.1.4 -->
-<!--    <script src="/adminlte/plugins/jQuery/jQuery-2.1.4.min.js"></script>-->
-<!--    <!-- jQuery UI 1.11.2 -->
-<!--    <script src="http://code.jquery.com/ui/1.11.2/jquery-ui.min.js" type="text/javascript"></script>-->
-<!--    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<!--    <script>-->
-<!--      $.widget.bridge('uibutton', $.ui.button);-->
-<!--    </script>-->
-<!--    <!-- Bootstrap 3.3.2 JS -->
-<!--    <script src="/adminlte/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>-->
-<!--    <!-- Morris.js charts -->
-<!--    <script src="http://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>-->
-<!--    <script src="/adminlte/plugins/morris/morris.min.js" type="text/javascript"></script>-->
-<!--    <!-- Sparkline -->
-<!--    <script src="/adminlte/plugins/sparkline/jquery.sparkline.min.js" type="text/javascript"></script>-->
-<!--    <!-- jvectormap -->
-<!--    <script src="/adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js" type="text/javascript"></script>-->
-<!--    <script src="/adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js" type="text/javascript"></script>-->
-<!--    <!-- jQuery Knob Chart -->
-<!--    <script src="/adminlte/plugins/knob/jquery.knob.js" type="text/javascript"></script>-->
-<!--    <!-- daterangepicker -->
-<!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js" type="text/javascript"></script>-->
-<!--    <script src="/adminlte/plugins/daterangepicker/daterangepicker.js" type="text/javascript"></script>-->
-<!--    <!-- datepicker -->
-<!--    <script src="/adminlte/plugins/datepicker/bootstrap-datepicker.js" type="text/javascript"></script>-->
-<!--    <!-- Bootstrap WYSIHTML5 -->
-<!--    <script src="/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js" type="text/javascript"></script>-->
-<!--    <!-- Slimscroll -->
-<!--    <script src="/adminlte/plugins/slimScroll/jquery.slimscroll.min.js" type="text/javascript"></script>-->
-<!--    <!-- FastClick -->
-<!--    <script src='/adminlte/plugins/fastclick/fastclick.min.js'></script>-->
-<!--    <!-- AdminLTE App -->
-<!--    <script src="/adminlte/dist/js/app.min.js" type="text/javascript"></script>-->
-<!--    -->
-<!--    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<!--    <script src="/adminlte/dist/js/pages/dashboard.js" type="text/javascript"></script>-->
-<!--    -->
-<!--    <!-- AdminLTE for demo purposes -->
-<!--    <script src="/adminlte/dist/js/demo.js" type="text/javascript"></script>-->
   </body>
 </html>
