997835eecfdbaa66d4ed240b8c2362901690afbf
Michael <misha.sunsetboy@gmail.com>
2020-03-07 11:45:12 +0300

    шаблоны

--- themes/2020/views/frontend/smart.php
+++ themes/2020/views/frontend/smart.php
@@ -1,5 +1,5 @@
 <?php
-CController::renderPartial('webroot.themes.2017.views.frontend.include._header');
+CController::renderPartial('webroot.themes.2020.views.frontend.include._header');
 ?>
 
 <div class="container">
