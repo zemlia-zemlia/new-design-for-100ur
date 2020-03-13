600cc8f87dd9c42e44ccea602b9f2c75b57244bb
andrey <andrey@spb-abris.ru>
2020-03-13 15:38:54 +0300

    фикс формы регистрации ( пустая форма, не юрист )

--- protected/migrations/20200310142518_add_column_to_yurist.php
+++ protected/migrations/20200310142518_add_column_to_yurist.php
@@ -6,7 +6,7 @@ class AddColumnToYurist extends AbstractMigration
 {
     public function up()
     {
-        $this->table('100_yuristsettings')
+        $this->table('100_yuristSettings')
             ->addColumn('inn', 'string', ['null' => true, 'comment' => 'ИНН', 'limit' => 11 ])
             ->addColumn('companyName', 'string', ['null' => true, 'comment' => 'Название компании', 'limit' => 255 ])
             ->addColumn('address', 'string', ['null' => true, 'comment' => 'Адрес', 'limit' => 255 ])
@@ -15,7 +15,7 @@ class AddColumnToYurist extends AbstractMigration
 
     public function down()
     {
-        $this->table('100_yuristsettings')
+        $this->table('100_yuristSettings')
             ->removeColumn('inn')
             ->removeColumn('companyName')
             ->removeColumn('address')
