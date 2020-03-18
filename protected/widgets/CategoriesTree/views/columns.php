72e1bb3dc8f29406a1145cc4d41bd2257e959a33
Vitalii <89269643535@mail.ru>
2020-03-18 16:15:08 +0300

    fix

--- protected/widgets/CategoriesTree/views/columns.php
+++ protected/widgets/CategoriesTree/views/columns.php
@@ -5,7 +5,7 @@
     <?php endif; ?>
     <div class="col-sm-4 vert-margin20">
         <?php if (isset($cat['icon'])) : ?>
-        <img src="/upload/category_icons/<?= $cat['icon'] ?>" width="50" alt="">
+        <img src="/upload/category_icons/<?= $cat['icon'] ?>" width="30" alt="">
         <?php endif; ?>
         <?php echo CHtml::link(CHtml::encode($cat['name']), Yii::app()->createUrl('questionCategory/alias', ['name' => CHtml::encode($cat['alias'])])); ?>
     </div>
