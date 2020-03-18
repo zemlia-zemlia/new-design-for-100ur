3920801f5421b67eece27f63a75e38d6c9f5f12b
andrey <andrey@spb-abris.ru>
2020-03-18 16:09:45 +0300

    вывести иконки на главную

--- protected/widgets/CategoriesTree/views/columns.php
+++ protected/widgets/CategoriesTree/views/columns.php
@@ -4,6 +4,9 @@
         <div class="row">
     <?php endif; ?>
     <div class="col-sm-4 vert-margin20">
+        <?php if (isset($cat['icon'])) : ?>
+        <img src="/upload/category_icons/<?= $cat['icon'] ?>" width="50" alt="">
+        <?php endif; ?>
         <?php echo CHtml::link(CHtml::encode($cat['name']), Yii::app()->createUrl('questionCategory/alias', ['name' => CHtml::encode($cat['alias'])])); ?>
     </div>
     <?php if (2 == $index % 3): ?>
