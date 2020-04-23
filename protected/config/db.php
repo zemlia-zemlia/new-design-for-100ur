e78ebcb17b039cc2158a659651368a0a44ab882e
Michael <misha.sunsetboy@gmail.com>
2020-04-23 19:15:37 +0300

    правка бага с конфигом db

--- protected/config/db.php
+++ protected/config/db.php
@@ -1,18 +1,14 @@
 <?php
 
 // параметры доступа к БД
-if (YII_DEV == true) {
-    return [
-        'connectionString' => 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
-        'emulatePrepare' => true,
-        'username' => getenv('DB_USER'),
-        'password' => getenv('DB_PASSWORD'),
-        'charset' => 'utf8',
-        'tablePrefix' => '100_',
-        'enableProfiling' => true,
-        'schemaCachingDuration' => 3000,
-        'queryCacheID' => 'cache',
-    ];
-} else {
-    // Настройки для продакшена..
-}
+return [
+    'connectionString' => 'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('DB_NAME'),
+    'emulatePrepare' => true,
+    'username' => getenv('DB_USER'),
+    'password' => getenv('DB_PASSWORD'),
+    'charset' => 'utf8',
+    'tablePrefix' => '100_',
+    'enableProfiling' => true,
+    'schemaCachingDuration' => 3000,
+    'queryCacheID' => 'cache',
+];
