af529917d374d7dd108bdbaa91e2487c0102f55b
Michael <misha.sunsetboy@gmail.com>
2020-05-02 17:31:53 +0300

    workers

--- phpsocket.io/server/chat/start.php
+++ phpsocket.io/server/chat/start.php
@@ -7,7 +7,7 @@ use PHPSocketIO\SocketIO;
 
 define('GLOBAL_START', true);
 
-// require_once __DIR__ . '/start_web.php';
+require_once __DIR__ . '/start_web.php';
 require_once __DIR__ . '/start_io.php';
 
 Worker::runAll();
