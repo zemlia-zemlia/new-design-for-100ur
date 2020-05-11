907422f2e83e77513dc2918227fc63732ae59bd6
Michael <misha.sunsetboy@gmail.com>
2020-05-11 20:43:26 +0300

    тест оплаты вопроса

--- protected/tests/integration/components/YandexPaymentResponseProcessorTest.php
+++ protected/tests/integration/components/YandexPaymentResponseProcessorTest.php
@@ -3,9 +3,11 @@
 namespace App\tests\integration\components;
 
 use App\models\Money;
+use App\models\Question;
 use App\models\TransactionCampaign;
 use App\models\User;
 use App\models\YaPayConfirmRequest;
+use Tests\Factories\QuestionFactory;
 use Tests\Factories\UserFactory;
 use Tests\integration\BaseIntegrationTest;
 use YandexPaymentResponseProcessor;
@@ -32,7 +34,7 @@ class YandexPaymentResponseProcessorTest extends BaseIntegrationTest
         ]);
 
         $this->loadToDatabase(User::getFullTableName(), [$userAttributes]);
-        $this->tester->seeInDatabase('100_user', ['id' => 100]);
+        $this->tester->seeInDatabase(User::getFullTableName(), ['id' => 100]);
 
         $requestData = [
             'label' => 'u-100',
@@ -59,6 +61,41 @@ class YandexPaymentResponseProcessorTest extends BaseIntegrationTest
         $this->tester->seeInDatabase(TransactionCampaign::getFullTableName(), [
             'sum' => 12500,
             'buyerId' => 100,
+        ]);
+    }
+
+    public function testPaymentForQuestion()
+    {
+        $questionFactory = new QuestionFactory();
+        $questionAttributes = $questionFactory->generateOne([
+            'id' => 100,
+            'payed' => 0,
+        ]);
+
+        $this->loadToDatabase(Question::getFullTableName(), [$questionAttributes]);
+        $this->tester->seeInDatabase(Question::getFullTableName(), ['id' => 100]);
+
+        $requestData = [
+            'label' => 'q-100',
+            'withdraw_amount' => 125,
+        ];
+        $secret = 'test_secret';
+        $yandexRequestData = new YaPayConfirmRequest();
+        $yandexRequestData->setAttributes($requestData);
+
+        $paymentProcessor = new YandexPaymentResponseProcessor($yandexRequestData, $secret, false);
+        $processResult = $paymentProcessor->process();
+
+        $this->assertTrue($processResult);
+        $this->tester->seeInDatabase(Question::getFullTableName(), [
+            'id' => 100,
+            'payed' => 1,
+            'price' => 12500,
+        ]);
+
+        $this->tester->seeInDatabase(Money::getFullTableName(), [
+            'type' => Money::TYPE_INCOME,
+            'direction' => 504,
         ]);
     }
 }
