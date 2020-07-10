4929406bd970514050e9c537d740342069efbeb2
andrey <andrey@spb-abris.ru>
2020-07-10 18:35:49 +0300

    question

--- protected/views/question/archiveAjax.php
+++ protected/views/question/archiveAjax.php
@@ -12,7 +12,7 @@
     'itemView' => '_viewArchive',
     'htmlOptions' => ['class' => 'archive-questions__body'],
     'ajaxUpdate' => true,
-    'pager' => ['class' => 'GTLinkPager'],
+    'pager' => ['class' => 'GTLinkPager', 'cssFile' => false],
     'pagerCssClass' => 'pagination',
     'template' => '{items} <a href="#" data-per_page="25" class="archive-questions__more-btn">Показать еще 25 вопросов</a> {pager}'
 ]); ?>
